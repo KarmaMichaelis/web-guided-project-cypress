5a2fc3cd09672d5d5d56642c3554518cf2dfdcfd
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-06-18 18:35:53 +0200

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,10 @@
 // write tests here
+describe('Inputs and submit button', () => { // grouping several tests that are related
+  it('can navigate to the site', () => { // this is the test
+    // remote control operation
+    cy.visit('http://localhost:1234')
+
+    // assertion
+    cy.url().should('include', 'http://localhost:1234')
+  })
+})
